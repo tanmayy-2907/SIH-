3323402d0f1aae0623b9a295fd9bc7e9ee3b77aa
Tanmay Narkar <166168950+tanmayy-2907@users.noreply.github.com>
2024-08-31 12:01:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//login
+
+document.getElementById('signInForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Simple validation
+    if (email === "" || password === "") {
+        alert("Please fill in all fields.");
+    } else {
+        
+
+        // Redirect to the main home page of the website
+        window.location.href = "home.html";  // Update this URL to match your home page
+    }
+});
+
+
+    
+
+
+
+
